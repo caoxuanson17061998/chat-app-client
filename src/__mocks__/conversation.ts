37078c9e5f01e03916831abb17989b2cd2c88723
caoxuanson17061998 <caoxuanson1706@gmail.com>
2023-02-16 16:01:28 +0700

    Added Mock Conversations & Styled Conversation Sidebar Item

---
+++ src/__mocks__/conversation.ts
@@ -0,0 +1,106 @@
+import { ConversationType } from '../utils/types';
+
+const mockConversations: ConversationType[] = [
+  {
+    id: 1,
+    name: 'Jimmy Anderson',
+    lastMessage: 'How are you???',
+  },
+  {
+    id: 2,
+    name: 'Hannah Michael',
+    lastMessage: 'See you at noon',
+  },
+  {
+    id: 3,
+    name: 'Billy Bob Jones',
+    lastMessage: 'I am at the library',
+  },
+  {
+    id: 4,
+    name: 'Connor McGregor',
+    lastMessage: 'How are you???',
+  },
+  {
+    id: 1,
+    name: 'Jimmy Anderson',
+    lastMessage: 'How are you???',
+  },
+  {
+    id: 2,
+    name: 'Hannah Michael',
+    lastMessage: 'See you at noon',
+  },
+  {
+    id: 3,
+    name: 'Billy Bob Jones',
+    lastMessage: 'I am at the library',
+  },
+  {
+    id: 4,
+    name: 'Connor McGregor',
+    lastMessage: 'How are you???',
+  },
+  {
+    id: 1,
+    name: 'Jimmy Anderson',
+    lastMessage: 'How are you???',
+  },
+  {
+    id: 2,
+    name: 'Hannah Michael',
+    lastMessage: 'See you at noon',
+  },
+  {
+    id: 3,
+    name: 'Billy Bob Jones',
+    lastMessage: 'I am at the library',
+  },
+  {
+    id: 4,
+    name: 'Connor McGregor',
+    lastMessage: 'How are you???',
+  },
+  {
+    id: 1,
+    name: 'Jimmy Anderson',
+    lastMessage: 'How are you???',
+  },
+  {
+    id: 2,
+    name: 'Hannah Michael',
+    lastMessage: 'See you at noon',
+  },
+  {
+    id: 3,
+    name: 'Billy Bob Jones',
+    lastMessage: 'I am at the library',
+  },
+  {
+    id: 4,
+    name: 'Connor McGregor',
+    lastMessage: 'How are you???',
+  },
+  {
+    id: 1,
+    name: 'Jimmy Anderson',
+    lastMessage: 'How are you???',
+  },
+  {
+    id: 2,
+    name: 'Hannah Michael',
+    lastMessage: 'See you at noon',
+  },
+  {
+    id: 3,
+    name: 'Billy Bob Jones',
+    lastMessage: 'I am at the library',
+  },
+  {
+    id: 4,
+    name: 'Connor McGregor',
+    lastMessage: 'How are you???',
+  },
+];
+
+export default mockConversations;
