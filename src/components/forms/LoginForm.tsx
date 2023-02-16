1e88aae664724a0ddd27a092d07b1b1751b8b373
caoxuanson17061998 <caoxuanson1706@gmail.com>
2023-02-16 15:25:11 +0700

    Add form validation for Register & Login forms

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,20 +1,39 @@
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import { Button, InputContainer, InputField, InputLabel } from '../../utils/styles';
 import styles from './index.module.scss';
 
 export const LoginForm = () => {
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  console.log(errors);
+
+  const onSubmit = (data: any) => {
+    console.log(data);
   };
+
   return (
-    <form className={styles.form} onSubmit={onSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <InputContainer>
         <InputLabel htmlFor="email">Email</InputLabel>
-        <InputField type="email" id="email" />
+        <InputField
+          type="email"
+          id="email"
+          {...register('email', { required: 'Email is Required' })}
+        />
       </InputContainer>
       <InputContainer className={styles.loginFormPassword}>
         <InputLabel htmlFor="password">Password</InputLabel>
-        <InputField type="password" id="password" />
+        <InputField
+          type="password"
+          id="password"
+          autoComplete="false"
+          {...register('password', { required: 'Password is Required' })}
+        />
       </InputContainer>
       <Button>Login</Button>
       <div className={styles.footerText}>
